51eed04e09153ab4fcc45baff91081f6818caf53
Michael <michael.d.barto@gmail.com>
2025-01-09 01:19:41 -0500

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "white",
+      }}
+    >
+      <Text style={{ fontSize: 24 }}>Aora</Text>
+      <Link href="/profile" style={{ color: "blue" }}>
+        Go to Profile
+      </Link>
+    </View>
+  );
+}
