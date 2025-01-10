495026a4608e0c27fc31458d668c90ecca10a978
Michael <michael.d.barto@gmail.com>
2025-01-10 18:56:34 -0500

    creating onboarding screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,55 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, ScrollView, Image } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.blackText}>Aora</Text>
-      <Link href="/home" style={styles.link}>
-        Go to Home
-      </Link>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.contentContainer}>
+        <View
+          style={{
+            width: "100%",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+            paddingLeft: 16,
+            paddingRight: 16,
+          }}
+        >
+          <Image
+            source={images.logo}
+            style={{
+              width: 130,
+              height: 130,
+            }}
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            style={{
+              width: 343,
+              height: 300,
+            }}
+            resizeMode="contain"
+          />
+          <View style={{ position: "relative", marginTop: 20 }}>
+            <Text
+              style={{
+                fontSize: 30, // Corresponds to text-3xl
+                color: "#ffffff", // Corresponds to text-white
+                fontWeight: "bold", // Corresponds to font-bold
+                textAlign: "center", // Corresponds to text-center
+              }}
+            >
+              Discover Endless Possibilities with{" "}
+              <Text style={{ color: "#FFA001" }}>Aora</Text>
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -18,12 +58,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "white",
+    backgroundColor: "black",
   },
+
+  contentContainer: {
+    height: "100%",
+  },
+
   blackText: {
     fontSize: 24,
     fontFamily: "Poppins-Black",
   },
+
   link: {
     color: "blue",
     fontFamily: "Poppins-Regular",
