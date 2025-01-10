14e30a34aeca4565307ecf0e23099cb2ed985283
Michael <michael.d.barto@gmail.com>
2025-01-10 12:37:56 -0500

    file structure

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,31 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Link } from "expo-router";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "white",
-      }}
-    >
-      <Text style={{ fontSize: 24 }}>Aora</Text>
-      <Link href="/profile" style={{ color: "blue" }}>
+    <View style={styles.container}>
+      <Text style={styles.blackText}>Aora</Text>
+      <Link href="/profile" style={styles.link}>
         Go to Profile
       </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  blackText: {
+    fontSize: 24,
+    fontFamily: "Poppins-Black",
+  },
+  link: {
+    color: "blue",
+    fontFamily: "Poppins-Regular",
+  },
+});
