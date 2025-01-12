066f2f74cb7bd96d6d5447c3c6d75769ca2a4ec1
Michael <michael.d.barto@gmail.com>
2025-01-12 18:12:03 -0500

    formfield component created

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,52 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  Dimensions,
+  Image,
+} from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import FormField from "@/components/FormField";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text> SignIn</Text>
-    </View>
+    <SafeAreaView style={{ backgroundColor: "black", height: "100%" }}>
+      <ScrollView>
+        <View
+          style={{
+            width: "100%",
+            justifyContent: "center",
+            height: "100%",
+            paddingHorizontal: 16,
+            marginVertical: 24,
+          }}
+        >
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            style={{
+              width: 115,
+              height: 34,
+            }}
+          />
+          <Text
+            style={{
+              fontSize: 24, // text-2xl
+              fontWeight: "600", // font-semibold
+              color: "#FFFFFF", // text-white
+              marginTop: 40, // mt-10
+              fontFamily: "Poppins-SemiBold", // font-psemibold
+            }}
+          >
+            Log in to Aora
+          </Text>
+          <FormField />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
---
+++ components/FormField.tsx
@@ -0,0 +1,20 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const FormField = () => {
+  return (
+    <View style={{ marginBottom: 8 }}>
+      <Text
+        style={{
+          fontSize: 16,
+          color: "#f3f4f6",
+          fontFamily: "Poppins-Medium",
+        }}
+      >
+        FormField
+      </Text>
+    </View>
+  );
+};
+
+export default FormField;
