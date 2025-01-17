fb31e3f61e8ea96273d658d96a0824e31a38af67
Michael <michael.d.barto@gmail.com>
2025-01-16 23:51:18 -0500

    started implementing SearchInput

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,15 +1,71 @@
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, Image } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import SearchInput from "@/components/SearchInput";
 
 const Home = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ backgroundColor: "black" }}>
       <FlatList
         data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
-          <Text style={{ fontSize: 30 }}>{item.id}</Text>
+          <Text style={{ fontSize: 30, color: "white" }}>{item.id}</Text>
+        )}
+        ListHeaderComponent={() => (
+          <View
+            style={{
+              flex: 1,
+              marginVertical: 24,
+              paddingHorizontal: 16,
+              gap: 24,
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "flex-start",
+                marginBottom: 24,
+              }}
+            >
+              <View>
+                <Text
+                  style={{
+                    fontFamily: "Poppins-Medium",
+                    fontSize: 14,
+                    color: "#f3f4f6",
+                  }}
+                >
+                  Welcome Back
+                </Text>
+                <Text
+                  style={{
+                    fontFamily: "Poppins-SemiBold",
+                    fontSize: 24,
+                    color: "#ffffff",
+                  }}
+                >
+                  JSMastery
+                </Text>
+              </View>
+              <View style={{ marginTop: 6 }}>
+                <Image
+                  style={{ width: 36, height: 40 }}
+                  source={images.logoSmall}
+                  resizeMode="contain"
+                />
+              </View>
+            </View>
+            <SearchInput
+              title={""}
+              value={""}
+              handleChangeText={function (text: string): void {
+                throw new Error("Function not implemented.");
+              }}
+            />
+          </View>
         )}
       />
     </SafeAreaView>
---
+++ components/SearchInput.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import {
+  View,
+  Image,
+  Text,
+  TextInput,
+  StyleProp,
+  ViewStyle,
+  TouchableOpacity,
+} from "react-native";
+import { icons } from "../constants";
+
+type FormFieldProps = {
+  title: string;
+  value: string;
+  placeholder?: string;
+  handleChangeText: (text: string) => void;
+  styles?: StyleProp<ViewStyle>;
+  keyboardType?: "default" | "email-address";
+};
+
+const SearchInput: React.FC<FormFieldProps> = ({
+  title,
+  value,
+  handleChangeText,
+  styles,
+  keyboardType = "default",
+  placeholder,
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <View
+      style={{
+        width: "100%",
+        height: 64,
+        paddingHorizontal: 16,
+        backgroundColor: "#161622",
+        borderRadius: 16,
+        borderWidth: 2,
+        borderColor: "#232533",
+        flexDirection: "row",
+        alignItems: "center",
+        marginTop: 8,
+      }}
+    >
+      <TextInput
+        value={value}
+        placeholder={placeholder}
+        placeholderTextColor={"#7b7b8b"}
+        onChangeText={handleChangeText}
+        keyboardType={keyboardType}
+        secureTextEntry={title === "Password" && !showPassword}
+        style={{
+          flex: 1,
+          color: "#FFFFFF",
+          fontSize: 16,
+          fontFamily: "Poppins-Regular",
+        }}
+      />
+
+      {title === "Password" && (
+        <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+          <Image
+            source={!showPassword ? icons.eye : icons.eyeHide}
+            style={{ width: 24, height: 24 }}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+};
+
+export default SearchInput;
